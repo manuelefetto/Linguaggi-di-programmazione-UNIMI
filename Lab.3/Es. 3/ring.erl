65baed99c8526200b93d4fa48561905fb765eba6
manuelefetto <manuele.ferro@studenti.unimi.it>
2025-11-06 18:23:23 +0100

    Added Lab. 3 Es. 3, but not working correctly

---
+++ Lab.3/Es. 3/ring.erl
@@ -0,0 +1,40 @@
+-module(ring).
+-export([start/3]).
+
+start(M,N, Message) -> 
+    io:format("Creo anello di ~p processi, numero di passaggi del messaggio: ~p~n", [N, M]),
+    Pids = create_ring(N),
+    [First | _] = Pids,
+    io:format("Anello creato. Messaggio iniziale ...~n", []),
+    First ! {pass, M, Message},
+    ok.
+
+create_ring(N)-> 
+    PIds = [spawn(ring , process, [])  ||_ <- lists:seq(1,N)],
+    link_rings(PIds),
+    PIds.
+
+link_rings(PIds) -> 
+    lists:zipwith(fun(P,Next) -> P ! {set_next, Next}end, PIds, tl(PIds) ++ hd(PIds)).
+
+process() ->
+    receive
+        {set_next, Next} ->
+            io:format("~p --> Imposto il prossimo processo a Pid: ~p ~n",[self(), Next]), 
+            loop(Next)
+    end.
+
+loop(PIdNext) -> 
+    receive
+        {pass, 0, Message} ->
+            io:format("~p --> Final message: < ~p > ~n Sendig quit message to ~p",[self(), Message, PIdNext]),
+            PIdNext ! {quit},
+            loop(PIdNext);
+        {pass, M, Message} -> 
+            io:format("~p --> Passing message: < ~p > to ~p~n",[self(), Message, PIdNext]),
+            PIdNext ! {pass, M-1, Message},
+            loop(PIdNext);
+        {quit} ->
+            PIdNext ! {quit}
+    end.
+
