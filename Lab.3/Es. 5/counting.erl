db8f3a32bdf40a1aa191c9791f81e2b660c31b23
manuelefetto <manuele.ferro@studenti.unimi.it>
2025-11-10 19:20:13 +0100

    Added Es. 5

---
+++ Lab.3/Es. 5/counting.erl
@@ -0,0 +1,25 @@
+-module(counting).
+-export([start/0,tot/0, dummy1/0,dummy2/0,dummy3/0]).
+
+start() -> register(count,spawn(fun loop/0)).
+
+tot() -> 
+    count ! { count, tot },
+    count ! { self(), result },
+    receive
+        {dictionary, Dict} -> io:format("~p~n",[Dict])
+    end.
+
+dummy1() -> io:format("Dummy 1~n",[]), count ! { count , dummy1 },ok.
+dummy2() -> io:format("Dummy 2~n",[]), count ! { count , dummy2 },ok.
+dummy3() -> io:format("Dummy 3~n",[]), count ! { count , dummy3 },ok.
+
+loop() ->
+    receive
+        {count, Func } -> case get(Func) of
+            undefined -> put(Func, 1);
+            N -> put(Func, N+1)
+        end,
+        loop();
+        { From, result } -> From ! process_info(self(), dictionary),loop()
+    end.
